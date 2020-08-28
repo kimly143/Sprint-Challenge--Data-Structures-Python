c6336f36331277dd280e69f3f6309b349deaa30c
Kim Ly Buck <kim.3993@gmail.com>
2020-08-28 18:37:08 -0500

    ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,18 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.write_pointer = 0
+        self.capacity = capacity
+        # create a list of the length with None in all it fields
+        self.buffer = [None] * self.capacity
 
     def append(self, item):
-        pass
+        self.buffer[self.write_pointer] = item
+        self.write_pointer += 1
+        self.write_pointer %= self.capacity 
 
     def get(self):
-        pass
+        output = []
+        for item in self.buffer:
+            if item is not None:
+                output.append(item)
+        return output
